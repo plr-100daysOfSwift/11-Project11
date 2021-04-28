df5cfddba74944694db697cc17514b44b05c4dcf
Paul Richardson <developer@photographerafoot.com>
2021-04-29 08:34:05 +1000

    CHG: add physics body to box and scene

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -15,12 +15,14 @@ class GameScene: SKScene {
 		background.blendMode = .replace
 		background.zPosition = -1
 		addChild(background)
+		physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
 	}
 	
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 		if let touch = touches.first {
 			let location = touch.location(in: self)
 			let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+			box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
 			box.position = location
 			addChild(box)
 		}
