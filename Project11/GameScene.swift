28f17a524f6a68504c2810a16e173c49e1f7c9c9
Paul Richardson <developer@photographerafoot.com>
2021-04-29 09:02:59 +1000

    CHG: remove extension from background file name
    the extension is not necessary when the file is in the assets catalog

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -10,7 +10,7 @@ import SpriteKit
 class GameScene: SKScene {
 	
 	override func didMove(to view: SKView) {
-		let background = SKSpriteNode(imageNamed: "background.jpg")
+		let background = SKSpriteNode(imageNamed: "background")
 		background.position = CGPoint(x: 512, y: 384)
 		background.blendMode = .replace
 		background.zPosition = -1
