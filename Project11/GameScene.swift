da45320b894d92751488d52d45d3a2a1f9318239
Paul Richardson <developer@photographerafoot.com>
2021-04-29 08:29:03 +1000

    CHG: implement touchesBegan
    add box

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -8,9 +8,6 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    
-    override func didMove(to view: SKView) {
-
 	
 	override func didMove(to view: SKView) {
 		let background = SKSpriteNode(imageNamed: "background.jpg")
@@ -19,10 +16,14 @@ class GameScene: SKScene {
 		background.zPosition = -1
 		addChild(background)
 	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		if let touch = touches.first {
+			let location = touch.location(in: self)
+			let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+			box.position = location
+			addChild(box)
 		}
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
-		}
-
+	}
+	
 }
