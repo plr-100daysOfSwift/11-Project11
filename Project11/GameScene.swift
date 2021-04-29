67267a8236b6f9e76e1c6c1bab03fa27fd9b8cd1
Paul Richardson <developer@photographerafoot.com>
2021-04-30 07:58:29 +1000

    CHG: call makeSlot()

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -17,11 +17,19 @@ class GameScene: SKScene {
 		addChild(background)
 		physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
 
+		makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
+		makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
+		makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
+		makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
+
+
 		makeBouncer(at: CGPoint(x: 0, y: 0))
 		makeBouncer(at: CGPoint(x: 256, y: 0))
 		makeBouncer(at	: CGPoint(x: 512, y: 0))
 		makeBouncer(at: CGPoint(x: 768, y: 0))
 		makeBouncer(at: CGPoint(x: 1024, y: 0))
+
+
 	}
 	
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
